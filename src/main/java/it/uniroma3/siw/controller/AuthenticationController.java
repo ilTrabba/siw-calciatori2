93f5a6d224e21a70d6527ff89d228ea39b7145b1
ilTrabba <cri.traballoni@gmail.com>
2023-08-06 17:42:09 +0200

    Authentication Controller

---
+++ src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -0,0 +1,90 @@
+package it.uniroma3.siw.controller;
+
+
+import it.uniroma3.siw.controller.validator.CredentialsValidator;
+import it.uniroma3.siw.controller.validator.UserValidator;
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.model.User;
+import it.uniroma3.siw.service.CredentialsService;
+import it.uniroma3.siw.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthenticationController {
+    @Autowired
+    private CredentialsService credentialsService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @Autowired
+    private CredentialsValidator credentialsValidator;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/index")
+    public String index(){
+        return "homePage.html";
+    }
+
+    @GetMapping(value = "/register")
+    public String showRegisterForm (Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("credentials", new Credentials());
+        return "formRegister.html";
+    }
+
+    @GetMapping(value = "/login")
+    public String showLoginForm (Model model) {
+        return "formLogin.html";
+    }
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "index.html";
+        }
+        else {
+            UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+            if(credentials.getRole().equals(Credentials.ADMIN_ROLE)){
+                return "admin/homePageAdmin.html";
+            } else {
+                return "index.html";
+            }
+        }
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@Valid @ModelAttribute("user") User user,
+                               BindingResult userBindingResult, @Valid
+                               @ModelAttribute("credentials") Credentials credentials,
+                               BindingResult credentialsBindingResult,
+                               Model model) {
+        this.userValidator.validate(user,userBindingResult);
+        this.credentialsValidator.validate(credentials, credentialsBindingResult);
+        // se user e credential hanno entrambi contenuti validi, memorizza User e the Credentials nel DB
+        if(!userBindingResult.hasErrors() && !credentialsBindingResult.hasErrors()) {
+            userService.saveUser(user);
+            credentials.setUser(user);
+            credentialsService.saveCredentials(credentials);
+            model.addAttribute("user", user);
+            return "formLogin.html";
+        }
+        return "formRegister.html";
+    }
+}
