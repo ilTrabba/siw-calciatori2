1b9e679bbd1153449e73ca38163d55be197fb291
ilTrabba <cri.traballoni@gmail.com>
2023-08-06 14:59:23 +0200

    Package Validator, userValidator, reviewValidator

---
+++ src/main/java/it/uniroma3/siw/controller/validator/UserValidator.java
@@ -0,0 +1,28 @@
+package it.uniroma3.siw.controller.validator;
+
+
+import it.uniroma3.siw.model.User;
+import it.uniroma3.siw.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        User user = (User) target;
+        if(user.getEmail() != null && this.userRepository.existsByEmail(user.getEmail())){
+            errors.reject("user.duplicate");
+        }
+    }
+}
